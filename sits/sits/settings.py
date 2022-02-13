b9301c64c3d2bc8364918b3fc19607bf028bff2f
Erjan18 <johnydepp2002@mail.ru>
2022-02-13 15:09:03 +0600

    commit

--- sits/sits/settings.py
+++ sits/sits/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'sits.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-        'USER': 'postgres',
-        'PASSWORD': 'qwertij',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
