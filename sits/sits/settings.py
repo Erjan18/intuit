bb3b022a0430d36ed7c1e7e75fabde2a688bb52c
Erjan18 <johnydepp2002@mail.ru>
2022-02-14 19:12:18 +0600

    commit

--- sits/sits/settings.py
+++ sits/sits/settings.py
@@ -110,8 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'era.kat2002@gmail.com'
-EMAIL_HOST_PASSWORD = '!Dethnote99'
+EMAIL_HOST_USER = 'redminote8prosuper@gmail.com'
+EMAIL_HOST_PASSWORD = 'panasonic1215'
 EMAIl_PORT = 587
 
 # Internationalization
